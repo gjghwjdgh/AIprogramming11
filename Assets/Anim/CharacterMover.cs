17b7d9291e6d3be4f069045ef30fba59065ff993
현민 <143337620+Ggulbogpig@users.noreply.github.com>
2025-06-01 04:31:47 +0900

    동작제어추가 - 공격모션
    Q : 휘두르기 / E : 발차기 / R: 회전베기로 각각 공격인덱스 0,1,2로 설정함

--- Assets/Anim/CharacterMover.cs
+++ Assets/Anim/CharacterMover.cs
@@ -27,6 +27,31 @@ public class RootMotionMover : MonoBehaviour
 
         // 애니메이터에 v 값만 전달 → 애니메이션 전환만 담당
         animator.SetFloat("v", v);
+
+        // Q 키 → 공격 모션 1 휘두르기
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetTrigger("attackTrigger");
+            animator.SetInteger("attackIndex", 0);
+            
+        }
+
+        // E 키 → 공격 모션 2 발차기
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetTrigger("attackTrigger");
+            animator.SetInteger("attackIndex", 1);
+            
+        }
+
+        // R 키 → 공격 모션 3 회전베기
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetTrigger("attackTrigger");
+            animator.SetInteger("attackIndex", 2);
+
+        }
+
     }
 
     void OnAnimatorMove()
