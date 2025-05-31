e75051998fb0de68fe5ba46c7dd5d02d962f18f8
현민 <143337620+Ggulbogpig@users.noreply.github.com>
2025-06-01 02:56:52 +0900

    동작 제어 추가 -전진
    w키 누르는 동안 전진/ 떼면 기본자세로 돌아감.

---
+++ Assets/Anim/CharacterMover.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class RootMotionMover : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.applyRootMotion = true;  //  반드시 켜야 함
+    }
+
+    void Update()
+    {
+        float v = 0.0f;
+
+        if (Input.GetKey(KeyCode.W))
+            v = 1.0f;
+        else
+        {
+            v = 0;
+        }
+
+        // 애니메이터에 v 값만 전달 → 애니메이션 전환만 담당
+        animator.SetFloat("v", v);
+    }
+
+    void OnAnimatorMove()
+    {
+        // Animator의 Root Motion 이동 데이터를 Transform에 누적 적용
+        transform.position += animator.deltaPosition;
+        transform.rotation *= animator.deltaRotation;
+    }
+}
